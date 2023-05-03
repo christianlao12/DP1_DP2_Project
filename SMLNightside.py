663dcb49dc992b61a250e89ff5be13d26a951406
Christian <kris12997@gmail.com>
2023-05-03 15:07:33 +0100

    update

---
+++ SMLNightside.py
@@ -0,0 +1,133 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+from matplotlib.dates import DateFormatter
+import seaborn as sns
+import statsmodels.api as sm
+
+sns.set_theme()
+sns.set_context("talk")
+sns.set_palette("colorblind")
+
+colors = sns.color_palette("colorblind")
+date_form = DateFormatter("%Y-%m-%d\n%H:%M")
+
+
+# %%
+sml_lt_df = pd.read_csv("Data/SML LT 2001.csv")
+sml_lt_df['Date_UTC'] = pd.to_datetime(sml_lt_df['Date_UTC'])
+
+sme_df = pd.read_csv("Data/SME 2001.csv")
+sme_df['Date_UTC'] = pd.to_datetime(sme_df['Date_UTC'])
+
+ng2011df = pd.read_csv("Data/substorms-newell-20000101_000000_to_20070101_000000.csv")
+ng2011df['Date_UTC'] = pd.to_datetime(ng2011df['Date_UTC'])
+ng2011_2001 = ng2011df[ng2011df['Date_UTC'].dt.year == 2001].reset_index(drop=True)
+
+# %%
+no_sectors = 7
+MLT_sectors = np.concatenate([np.arange((24-np.floor(no_sectors/2)), 24), np.arange(0, no_sectors/2)]).ravel().astype(int)
+allsec = [i for i in np.arange(0,24)]
+allsec_names = [f'SMLr{i:02d}' for i in allsec]
+MLT_sec_names =  [f'SMLr{i:02d}' for i in MLT_sectors]
+SMLnightdf = pd.DataFrame({'Date_UTC':sml_lt_df['Date_UTC'],'SMLnight':sml_lt_df[MLT_sec_names].min(axis=1)})
+SMLalldf = pd.DataFrame({'Date_UTC':sml_lt_df['Date_UTC'],'SMLall':sml_lt_df[allsec_names].min(axis=1)})
+
+# %%
+smlnight_scaled =((SMLnightdf['SMLnight'] - SMLnightdf['SMLnight'].mean())/SMLnightdf['SMLnight'].std()).to_numpy()
+sml_scaled = ((sme_df['SML'] - sme_df['SML'].mean())/sme_df['SML'].std()).to_numpy()
+
+
+# %%
+sml5_acorr = sm.tsa.stattools.acf(smlnight_scaled, nlags=12*60)
+sml_acorr = sm.tsa.stattools.acf(sml_scaled, nlags=12*60)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+ax.plot(sml5_acorr, label='SMLnight')
+ax.plot(sml_acorr, label='SML')
+ax.set_xlabel('Lag (min)')
+ax.set_ylabel('Correlation')
+ax.legend()
+ax.set_title('Autocorrelation of SMLnight and SML')
+
+plt.tight_layout(pad=1)
+
+# %%
+onsettimes = []
+onsetsectors = []
+minimas = []
+minimatimes = []
+sml_array = sml_lt_df[MLT_sec_names].values
+
+untilnext = 0
+for index, __ in SMLnightdf[:-30].iterrows():
+    onsettime = SMLnightdf['Date_UTC'][index]
+    onsetsml = SMLnightdf['SMLnight'][index]
+    onsetsector = np.where(sml_array[index]==onsetsml)[0]
+    if untilnext > 1:
+        untilnext -= 1
+        continue
+    if (SMLnightdf['SMLnight'][index + 1] - onsetsml) < -15:
+        if (SMLnightdf['SMLnight'][index + 2] - onsetsml) < -30:
+            if (SMLnightdf['SMLnight'][index + 3] - onsetsml) < -45:
+                window = SMLnightdf['SMLnight'][index + 4:index + 30]
+                if np.sum(window)/26 - onsetsml < -100:
+                    onsettimes.append(onsettime)
+                    onsetsectors.append(', '.join([str(element) for element in MLT_sectors[onsetsector]]))
+                    minimas.append(window.min())
+                    minimatimes.append(SMLnightdf['Date_UTC'][window.idxmin()])
+                    untilnext = 20
+    else:
+        untilnext -= 1
+        
+ng2011_night = pd.DataFrame({'Date_UTC':onsettimes, 'MLT_onset':onsetsectors, 'SML_Minima': minimas, 'Date_UTC_minima': minimatimes})
+
+# %%
+fig,ax = plt.subplots(figsize=(21, 7))
+
+t_diff = np.setdiff1d(ng2011_night['Date_UTC'], ng2011_2001['Date_UTC'])[0]
+tstart = t_diff - pd.Timedelta('45 minutes')
+tend = t_diff + pd.Timedelta('45 minutes')
+
+x = SMLnightdf['Date_UTC'][SMLnightdf['Date_UTC'].between(tstart,tend)]
+y = SMLnightdf['SMLnight'][SMLnightdf['Date_UTC'].between(tstart,tend)]
+x1 = sme_df['Date_UTC'][sme_df['Date_UTC'].between(tstart,tend)]
+y1 = sme_df['SML'][sme_df['Date_UTC'].between(tstart,tend)]
+
+ax.plot(x,y,label='SMLnight')
+ax.plot(x1,y1,label='SML')
+ax.axvline(pd.to_datetime(t_diff),c='k',ls='--',label='NG2011 Night MLT Onset')
+ax.set_xlabel('Time')
+ax.set_ylabel('SML (nT)')
+ax.legend()
+ax.set_title('SMLnight Onset but no SML onset')
+ax.xaxis.set_major_formatter(date_form)
+
+# %%
+fig,ax = plt.subplots(figsize=(21, 7))
+
+t_diff = np.setdiff1d(ng2011_2001['Date_UTC'], ng2011_night['Date_UTC'])[0]
+tstart = t_diff - pd.Timedelta('45 minutes')
+tend = t_diff + pd.Timedelta('45 minutes')
+
+x = SMLnightdf['Date_UTC'][SMLnightdf['Date_UTC'].between(tstart,tend)]
+y = SMLnightdf['SMLnight'][SMLnightdf['Date_UTC'].between(tstart,tend)]
+x1 = sme_df['Date_UTC'][sme_df['Date_UTC'].between(tstart,tend)]
+y1 = sme_df['SML'][sme_df['Date_UTC'].between(tstart,tend)]
+
+ax.plot(x,y,label='SMLnight')
+ax.plot(x1,y1,label='SML')
+ax.axvline(pd.to_datetime(t_diff),c='k', ls='--', label='NG2011 Original Onset')
+ax.set_xlabel('Time')
+ax.set_ylabel('SML (nT)')
+ax.legend()
+ax.set_title('SML onset but no SMLnight onset')
+ax.xaxis.set_major_formatter(date_form)
+
+# %%
+
+
+
---
+++ SubstormTypeDistributions.py
@@ -0,0 +1,69 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+from matplotlib.dates import DateFormatter
+import seaborn as sns
+
+sns.set_theme()
+sns.set_context("paper")
+sns.set_palette("colorblind")
+
+
+# %%
+# Loading in SOPHIE Data
+sophie80df = pd.read_csv("Data/SOPHIE_EPT80_1990-2022.csv", low_memory=False)
+sophie80df['Date_UTC'] = pd.to_datetime(sophie80df['Date_UTC'])
+sophie80df = sophie80df[sophie80df['Date_UTC'].between('1996','2021')].reset_index(drop=True)
+sophie80df['Delbay'] = pd.to_numeric(sophie80df['Delbay'],errors='coerce')
+
+array = np.zeros(len(sophie80df['Date_UTC']),dtype=int)
+for i in range(1,len(sophie80df['Date_UTC'])-2):
+    if (sophie80df.iloc[i-1]['Phase'] == 1) and (sophie80df.iloc[i]['Phase'] == 2) and (sophie80df.iloc[i]['Flag'] == 0) and (sophie80df.iloc[i+1]['Phase'] == 3) and (sophie80df.iloc[i+2]['Phase'] == 1):
+        array[i] = 1 # GERG
+        continue
+    if (sophie80df.iloc[i-1]['Phase'] == 1) and (sophie80df.iloc[i]['Phase'] == 2) and (sophie80df.iloc[i]['Flag'] == 0) and (sophie80df.iloc[i+1]['Phase'] == 3) and (sophie80df.iloc[i+2]['Phase'] == 2):
+        array[i] = 2 # GERE
+        continue
+    else:
+        array[i] = 0
+        continue
+sophie80df['Isolated Type'] = array
+
+
+# %%
+onlyonsets_wt = np.diff(sophie80df['Date_UTC'][sophie80df['Isolated Type'] == 1])/pd.to_timedelta(1, unit='h')
+onlyonsets_wt_10 = onlyonsets_wt[onlyonsets_wt < 10]
+
+onsets_extra_wt = np.diff(sophie80df['Date_UTC'][sophie80df['Isolated Type'] == 2])/pd.to_timedelta(1, unit='h')
+onsets_extra_wt_10 = onsets_extra_wt[onsets_extra_wt < 10]
+
+fig, axes = plt.subplots(figsize=(10,6))
+
+sns.histplot(onlyonsets_wt_10, bins=np.arange(0,10.25,0.25), ax=axes, stat='percent', label='G|ERG EPT80: Mean: {:.2f}, Std. Dev: {:.2f}, Median: {:.2f}'.format(np.nanmean(onlyonsets_wt_10),np.nanstd(onlyonsets_wt_10),np.nanmedian(onlyonsets_wt_10)))
+sns.histplot(onsets_extra_wt_10, bins=np.arange(0,10.25,0.25), ax=axes, stat='percent', label='G|ER... EPT80: Mean: {:.2f}, Std. Dev: {:.2f}, Median: {:.2f}'.format(np.nanmean(onsets_extra_wt_10),np.nanstd(onsets_extra_wt_10),np.nanmedian(onsets_extra_wt_10)))
+axes.xaxis.set_major_locator(ticker.MultipleLocator(1))
+axes.set_ylim(0,14.5)
+axes.legend(loc='center right')
+axes.set_xlabel('Waiting Time (Hours)')
+axes.set_ylabel('Probability (%)')
+axes.set_xlim(0,10)
+
+
+# %%
+onsetsonlysize = -sophie80df['Delbay'][sophie80df['Isolated Type'] == 1]
+onsetextrasize = -sophie80df['Delbay'][sophie80df['Isolated Type'] == 2]
+
+fig, axes = plt.subplots(figsize=(10,6),sharex=True,sharey=True)
+
+sns.histplot(onsetsonlysize, ax=axes, stat='percent', label='G|ERG EPT80: Mean: {:.2f}, Std. Dev: {:.2f}, Median: {:.2f}'.format(np.nanmean(onsetsonlysize),np.nanstd(onsetsonlysize),np.nanmedian(onsetsonlysize)))
+sns.histplot(onsetextrasize, ax=axes, stat='percent', label='G|ER... EPT80: Mean: {:.2f}, Std. Dev: {:.2f}, Median: {:.2f}'.format(np.nanmean(onsetextrasize),np.nanstd(onsetextrasize),np.nanmedian(onsetextrasize)))
+axes.legend(loc='center right')
+axes.set_xlabel('Substorm size (nT)')
+axes.set_ylabel('Probability (%)')
+
+# %%
+sophie80df[sophie80df['Isolated Type'] == 1]
+
+
